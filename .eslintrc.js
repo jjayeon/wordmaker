2780ffb4c395b88c7e87eb19f89aa4f76adb6ee0
jjayeon <autumn.chiu.136@gmail.com>
2021-03-18 13:14:34 -0400

    updated to include react, css loader, style loader, file loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     env: {
-	browser: true,
-	es2020: true,
+        browser: true,
+        es2020: true,
     },
-    extends: [
-	"eslint:recommended",
-	"prettier"
-    ],
+    extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
     parserOptions: {
-	ecmaVersion: 11,
-	sourceType: "module",
+        ecmaVersion: 11,
+        sourceType: "module",
     },
     rules: {},
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,48 @@
+/* eslint-disable no-undef */
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const PrettierWebpackPlugin = require("prettier-webpack-plugin");
 
 module.exports = {
-  devtool: "source-map",
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
+    devtool: "source-map",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                loader: "file-loader",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new PrettierWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new CleanWebpackPlugin(),
-    new PrettierWebpackPlugin(),
-  ],
-  devServer: {
-    contentBase: "./dist",
-  },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+    devServer: {
+        contentBase: "./dist",
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
 };
